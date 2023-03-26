ca60e9bb60d65a49147806624de8454d0c3900f2
张杰 <527512749@qq.com>
2023-03-26 16:55:25 +0800

    Revert "feat: 配置隐藏过滤的class"
    This reverts commit d92ca4b152db03eefb748f94eff8262b7c3cb72f.

--- LookinClient/Hierarchy/LKHierarchyDataSource.m
+++ LookinClient/Hierarchy/LKHierarchyDataSource.m
@@ -185,13 +185,6 @@ NSString * const LKHierarchyDataSourceReloadHierarchyNotification = @"LKHierarch
                 isSwiftProject = YES;
             }
         }
-        
-        // 隐藏需要过滤的class
-        if (info.filterShowClassList.count) {
-            if ([info.filterShowClassList containsObject:obj.title]) {
-                obj.isHidden = true;
-            }
-        }
     }];
     
     self.flatItems = flatItems;
