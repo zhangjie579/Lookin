798aede4a63fe28bd7c464dc06dec6fa75c74b5b
张杰 <527512749@qq.com>
2024-01-09 19:02:28 +0800

    feat: 增加查询view的祖先特定值的方法说明

--- LookinClient/Dashboard/Custom/KcCustomAttributesGroup.m
+++ LookinClient/Dashboard/Custom/KcCustomAttributesGroup.m
@@ -28,10 +28,11 @@
         debugMethodAttribute.identifier = LookinAttr_Kc_Debug_methodDesc;
         debugMethodAttribute.attrType = LookinAttrTypeCustomObj;
         debugMethodAttribute.value = @[
-            @"😄😄关于动态执行方法说明\n * 格式oc方法 [0x12434534/类名 方法名:参数value]\n * 支持参数: self/this, int...基本数据类型, string, 16进制address, 传递class用类名, @id(类名) -> 内部会创建对应对象, @id(地址) -> 内存会转objc对象",
+            @"😄😄关于动态执行方法说明\n * 格式oc方法 [0x12434534/类名 方法名:参数value]\n * 支持参数: self/this, int...基本数据类型, string, 16进制address, 传递class用类名, @id(类名) -> 内部会创建对应对象, @id(地址) -> 内存会转objc对象 (string不用写\"\", 否则会出错⚠️)",
             @"dump功能: 查看KcDebugSwift库 NSObject+KcObjcDump",
             
             @"查找属性name: kc_debug_findUIPropertyName",
+            @"通过keyPath查找值为value的所有祖先: [self kc_log_findAncestorViewValue:(id)value keyPath:clipsToBounds]",
             @"查找图层树下设置了对应属性(背景色: 0, 圆角: 1, 边框: 2)的所有subviews: [0x12434534(view的地址) matchSubviewsWithPropertyType:0]",
             @"dump对象: kc_dumpSwift",
             @"获取所有成员变量: kc_dump_allIvarDescription",
